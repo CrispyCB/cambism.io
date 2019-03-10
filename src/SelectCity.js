10525e6d68826afa79e5704241ca50af61ace4d1
Christian Bahnweg <cebahnweg@gmail.com>
2019-03-10 11:59:23 -0400

    creates SelectCity component

---
+++ src/SelectCity.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import "./Header.css"
+
+class SelectCity extends Component {
+    render() {
+      return (
+        <div className="Header">
+            <header className="header-inside">
+            Select your city:
+            </header>
+        </div>
+      );
+    }
+  }
+  
+  export default SelectCity;
