fb2945e57612c4c26111f837def83683c78f528b
Christian Bahnweg <cebahnweg@gmail.com>
2019-03-24 10:26:28 -0400

    creates simple dropdown

--- src/SelectCity.js
+++ src/SelectCity.js
@@ -8,6 +8,14 @@ class SelectCity extends Component {
             <header className="header-inside">
             Select your city:
             </header>
+            <div className="dropdown">
+              <span><a href={"#"}>Baltimore, MD</a></span>
+              <div className="dropdown-content">
+              <span><a href={"#"}>Philadelphia, PA</a></span>
+              <span><a href={"#"}>Pittsburgh, PA</a></span>
+              <span><a href={"#"}>New York City, NY</a></span>
+              </div>
+            </div>
         </div>
       );
     }
