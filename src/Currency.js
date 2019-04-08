3f1342bc67ba1a6b47b51d4fa45657635879d8b1
Christian Bahnweg <cebahnweg@gmail.com>
2019-04-07 20:44:47 -0400

    wipes currency.js clean for rebuild

--- src/Currency.js
+++ src/Currency.js
@@ -4,9 +4,6 @@ class Currency extends Component {
     render() {
         return (
             <div>
-                <span>
-                    <button onClick={this.props.handler}>{this.props.coin}</button> 
-                </span>
             </div>
         )
     }
