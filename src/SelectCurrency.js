68477ef7ed75208f3c81ed72b29513848fed300e
Christian Bahnweg <cebahnweg@gmail.com>
2019-04-07 20:17:01 -0400

    sets up selections properly, including currency rendering.

--- src/SelectCurrency.js
+++ src/SelectCurrency.js
@@ -1,23 +1,43 @@
 import React, { Component } from "react"
 import "./Header.css"
+import Currency from "./Currency";
 
 class SelectCurrency extends Component {
+    constructor (props) {
+      super(props)
+      this.select = this.select.bind(this)
+      this.state = {
+        currencySelected: false,
+        coin: null,
+      }
+    }
+    select(e){
+      this.setState({
+        currencySelected: true,
+        coin: e.target.value
+      })
+    }
     render() {
+      if(this.state.coin != null){
+        return (
+          <Currency coin={this.state.coin}/>
+        )
+      }
       
       return (
         <div className="Header">
             <header className="header-inside">
             Select your currency:
             </header>
-            <select>
-              <option value="90">Bitcoin</option>
-              <option value="285">Bitcoin Gold</option>
-              <option value="32073">BitCoin Diamond</option>
-              <option value="80">Ethereum</option>
-              <option value="">Ethereum Classic</option>
-              <option value="58">Ripple/XRP</option>
-              <option value="2679">EOS</option>
-              <option value="1">LiteCoin</option>
+            <select value={this.state.coin} onChange={this.select}>
+              <option value="90" >Bitcoin</option>
+              <option value="285" >Bitcoin Gold</option>
+              <option value="32073" >BitCoin Diamond</option>
+              <option value="80" >Ethereum</option>
+              <option value="118" >Ethereum Classic</option>
+              <option value="58" >Ripple/XRP</option>
+              <option value="2679" >EOS</option>
+              <option value="1" >LiteCoin</option>
             </select>
         </div>
       );
